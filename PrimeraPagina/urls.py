6bb948e00c3732bb0279e8faf7b8db8d742cb560
Daniel Greyling <64617322+ch4il4tt3@users.noreply.github.com>
2024-12-19 22:20:19 -0300

    create urls.py, views.py

--- PrimeraPagina/urls.py
+++ PrimeraPagina/urls.py
@@ -17,6 +17,8 @@ Including another URLconf
 from django.contrib import admin
 from django.urls import path
 
+from control_combustible import *
+
 urlpatterns = [
     path('admin/', admin.site.urls),
 ]
